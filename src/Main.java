5eee5cb3b62c557cc433d25b9328cf3587fb7c98
Asif <asifzamanzisan@gmail.com>
2024-12-12 19:21:48 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main{
 	
     public static void main(String[] args) {
         MainFrame home = new MainFrame();
-        System.out.println("Hello");
+        // System.out.println("Hello");
     }
 }
